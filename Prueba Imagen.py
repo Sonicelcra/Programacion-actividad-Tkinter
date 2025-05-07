d2f69200c0fe7d62027a085f2560e67fffdc5792
Juan Ignacio Rodriguez <72052039+Sonicelcra@users.noreply.github.com>
2025-05-07 09:18:37 -0300

    Add files via upload

---
+++ Prueba Imagen.py
@@ -0,0 +1,39 @@
+import tkinter as tk
+from urllib.request import urlopen
+from PIL import Image, ImageTk
+from io import BytesIO
+
+def descargarFondo():
+    urlImagen = "https://github.com/Sonicelcra/Imagenes/blob/main/Bosque%20Bonito.png?raw=true"
+    datosImagen = urlopen(urlImagen)
+    imagenBinaria = datosImagen.read()
+    imagen = Image.open(BytesIO(imagenBinaria))
+    return imagen
+
+def menuPrincipal():
+    
+    imagen=descargarFondo()
+    
+    imagenRedimensionada = imagen.resize((ancho, alto), Image.Resampling.LANCZOS)
+    
+    imagenTK = ImageTk.PhotoImage(imagenRedimensionada)
+
+    etiqueta = tk.Label(vt, image=imagenTK)
+    etiqueta.pack()
+    
+    vt.mainloop()
+
+def main():
+    global vt 
+    vt = tk.Tk()
+    vt.title("Graficacion")
+    
+    global ancho 
+    ancho = vt.winfo_screenwidth()
+    global alto
+    alto = vt.winfo_screenheight()
+    vt.geometry(f"{ancho}x{alto}")
+    menuPrincipal()
+    
+if __name__ == "__main__":
+    main()
